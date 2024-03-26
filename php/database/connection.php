23559b2dc377d022ae5f928ab54e4a0789f68b24
Soluções Software <ssolucoessoftware@gmail.com>
2024-03-26 15:42:44 -0300

    ✨  feat: Ajuste servidor e local

--- php/database/connection.php
+++ php/database/connection.php
@@ -4,7 +4,7 @@ try {
     //local
     $pdo = new PDO("sqlite:" . 'D:\laragon\www\a_php\todoList\php\database\database.sqlite');
     //servidor
-    // $pdo = new PDO("sqlite:" . $_SERVER['DOCUMENT_ROOT'].'/database/database.sqlite');
+    // $pdo = new PDO("sqlite:" . $_SERVER['DOCUMENT_ROOT'].'/php/database/database.sqlite');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Erro na conexão com: sqlite: ".$e->getMessage();
