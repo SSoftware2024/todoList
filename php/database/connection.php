bf696a56b2f5f8acef48707b5d98fdd7b8069fb8
Soluções Software <ssolucoessoftware@gmail.com>
2024-03-26 15:31:38 -0300

    ✨  feat: Ajuste servidor e local
    banco com seed

--- php/database/connection.php
+++ php/database/connection.php
@@ -1,7 +1,10 @@
 <?php
 
 try {
+    //local
     $pdo = new PDO("sqlite:" . 'D:\laragon\www\a_php\todoList\php\database\database.sqlite');
+    //servidor
+    // $pdo = new PDO("sqlite:" . $_SERVER['DOCUMENT_ROOT'].'/database/database.sqlite');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Erro na conexão com: sqlite: ".$e->getMessage();
